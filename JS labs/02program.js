87beaddc943815cccbf049682795305c20e19d98
shreyalab <shreyabashetty@gmail.com>
2025-12-27 05:58:21 -0800

    how functions work in js

---
+++ JS labs/02program.js
@@ -0,0 +1,15 @@
+console.log("02 program starts");
+var x = 1;
+a();
+b();
+console.log(x);
+
+function a() {
+    var x = 10;
+    console.log(x);
+}
+
+function b() {
+    var x = 100;
+    console.log(x);
+}
